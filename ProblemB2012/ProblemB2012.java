5f90cf7153e155a166b39afb80e820efa01c021c
Joseph Peyton <j.l.peyton@gmail.com>
2015-10-21 18:40:04 -0400

    updates

--- ProblemB2012/ProblemB2012.java
+++ ProblemB2012/ProblemB2012.java
@@ -10,20 +10,13 @@ public class ProblemB2012 {
 	int numNodesReviewed = 0;
 	boolean isDone = false;
 
-	public void init() {
+	public int init(String line, BufferedReader br) {
+		components = new ArrayList<CompNode>();
+		engineers = new ArrayList<EngNode>();
 
+		int numCompKinds = Integer.parseInt(line.split(" ")[0]);
+		int numEngKinds = Integer.parseInt(line.split(" ")[1]);
 		try {
-
-			BufferedReader br = new BufferedReader(new FileReader("input.txt"));
-
-			components = new ArrayList<CompNode>();
-			engineers = new ArrayList<EngNode>();
-
-			String line = br.readLine();
-
-			int numCompKinds = Integer.parseInt(line.split(" ")[0]);
-			int numEngKinds = Integer.parseInt(line.split(" ")[1]);
-
 			for(int i = 0;i < numCompKinds;i++) {
 				line = br.readLine();
 				int numComps = Integer.parseInt(line.split(" ")[0]);
@@ -32,7 +25,7 @@ public class ProblemB2012 {
 				for(int x = 0;x < numComps;x++) {
 					CompNode node = new CompNode(numReviews);
 
-					if(components.get(x-1) != null) {
+					if(x > 0) {
 						components.get(x-1).next = node;
 					}
 
@@ -48,7 +41,7 @@ public class ProblemB2012 {
 				for(int x = 0;x < numEngs;x++) {
 					EngNode node = new EngNode(numReviews);
 
-					if(engineers.get(x-1) != null) {
+					if(x > 0) {
 						engineers.get(x-1).next = node;
 					}
 
@@ -58,6 +51,7 @@ public class ProblemB2012 {
 
 			int nodeIndex = 0;
 			int numNodesReviewed = 0;
+			int numEngsLeft = engineers.size();
 
 			boolean isDone = false;
 
@@ -65,35 +59,70 @@ public class ProblemB2012 {
 				CompNode currentNode = components.get(nodeIndex);
 
 				if(currentNode.reviewsLeft > 0) {
-					for(int i = 0;i < engineers.size();i++) {
-						boolean hasVisted = false;
-						for(int x = 0;x < engineers.get(i).getVisitedNodes().size();x++) {
-							if(engineers.get(i).getVisitedNodes().get(x) == currentNode) {
-								hasVisted = true;
+					if(numEngsLeft > 0) {
+						for(int i = 0;i < engineers.size();i++) {
+							boolean hasVisted = false;
+							if (engineers.get(i).reviewsLeft > 0) {
+								for(int x = 0;x < engineers.get(i).getCompsVisited().size();x++) {
+									if(engineers.get(i).getCompsVisited().get(x) == currentNode) {
+										hasVisted = true;
+									}
+								}
+
+								if(!hasVisted && currentNode.reviewsLeft > 0) {
+									engineers.get(i).reviewsLeft--;
+									currentNode.reviewsLeft--;
+									engineers.get(i).visitedComps.add(currentNode);
+								}
+								else if(currentNode.reviewsLeft == 0) {
+									numNodesReviewed++;
+								}
+							}
+							else {
+								numEngsLeft--;
 							}
 						}
-
-						if(!hasVisted) {
-							engineers.get(i).reviewsLeft--;
-							currentNode.reviewsLeft--;
-							engineers.get(i).visitedNodes.add(currentNode);
-						}
+					}
+					else {
+						isDone = true;
+						System.out.println(0);
+						return 0;
 					}
 				}
+				else {
+					numNodesReviewed++;
+					System.out.println("nodes reviewed = " + numNodesReviewed + " Total nodes = " + components.size());
+					System.out.println("review remaining = " + currentNode.reviewsLeft);
+					if (numNodesReviewed == components.size()) {
+						isDone = true;
+						return 1;
+					}
+				}
+				nodeIndex++;
+				if (nodeIndex >= components.size()) {
+					nodeIndex = 0;
+				}
 			}
-
 		}
 		catch(Exception e) {
 			e.printStackTrace();
 		}
+		return 0;
 	}
-
+	
 	public static void main(String[] args) {
-
-	ProblemB2012 problem = new ProblemB2012();
-
-	problem.init();
-
+		ProblemB2012 problem = new ProblemB2012();
+		try {
+			BufferedReader br = new BufferedReader(new FileReader("input.txt"));
+			String line = br.readLine();
+			while (!line.equals("0 0") && line != null) {
+				System.out.println(problem.init(line, br));
+				line = br.readLine();
+			}
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
 
