8d6238ad6c63f5fb3b791c3dd505adc6ea8a7574
juanvallejo <juan.vallejo.12@cnu.edu>
2015-10-26 03:05:06 -0400

    add debug log

--- ProblemB2012/ProblemB2012.java
+++ ProblemB2012/ProblemB2012.java
@@ -13,7 +13,7 @@ public class ProblemB2012 {
 	public int init(String line, BufferedReader br) {
 		components = new ArrayList<CompNode>();
 		engineers = new ArrayList<EngNode>();
-
+System.out.println("-------------");
 		int numCompKinds = Integer.parseInt(line.split(" ")[0]);
 		int numEngKinds = Integer.parseInt(line.split(" ")[1]);
 		try {
@@ -29,6 +29,7 @@ public class ProblemB2012 {
 						components.get(x-1).next = node;
 					}
 
+					// System.out.println("Adding node component");
 					components.add(node);
 				}
 			}
@@ -39,6 +40,7 @@ public class ProblemB2012 {
 				int numReviews = Integer.parseInt(line.split(" ")[1]);
 
 				for(int x = 0;x < numEngs;x++) {
+					
 					EngNode node = new EngNode(numReviews);
 
 					if(x > 0) {
@@ -61,46 +63,67 @@ public class ProblemB2012 {
 				if(currentNode.reviewsLeft > 0) {
 					if(numEngsLeft > 0) {
 						for(int i = 0;i < engineers.size();i++) {
-							boolean hasVisted = false;
+							Log.log("Now at node # " + nodeIndex + " (" + currentNode.reviewsLeft + " reviews left)");
+							Log.log("Now at engineer # " + i + " (" + engineers.get(i).reviewsLeft + " reviews left)");
+
+							boolean hasVisited = false;
 							if (engineers.get(i).reviewsLeft > 0) {
 								for(int x = 0;x < engineers.get(i).getCompsVisited().size();x++) {
 									if(engineers.get(i).getCompsVisited().get(x) == currentNode) {
-										hasVisted = true;
+										hasVisited = true;
 									}
 								}
 
-								if(!hasVisted && currentNode.reviewsLeft > 0) {
+								if(!hasVisited && currentNode.reviewsLeft > 0) {
+									Log.log("Node " + nodeIndex + " was not visited, visiting");
 									engineers.get(i).reviewsLeft--;
 									currentNode.reviewsLeft--;
 									engineers.get(i).visitedComps.add(currentNode);
-								}
-								else if(currentNode.reviewsLeft == 0) {
+									Log.log("Node reviews left = " + currentNode.reviewsLeft + "; Engineer reviews left = " + engineers.get(i).reviewsLeft);
+									Log.log("----------");
+								} else if(!hasVisited && currentNode.reviewsLeft == 0) {
+									Log.log("Node " + nodeIndex + " has not been visited by engineer " + i + " but has no reviews left, incrementing node count...");
 									numNodesReviewed++;
 								}
+
+								if(hasVisited) {
+									Log.log("Node " + nodeIndex + " has already been visited by this engineer, ignoring");
+								}
+
+								Log.log("\nAmount of nodes visited = " + numNodesReviewed + "\n");
+									
 							}
 							else {
+								System.out.println(numEngsLeft + " engineers left");
 								numEngsLeft--;
 							}
 						}
 					}
 					else {
 						isDone = true;
-						System.out.println(0);
-						return 0;
+						if(numNodesReviewed == components.size()) {
+							Log.log("\nNo engineers left. All nodes have been reviewed\n");
+							return 1;
+						} else {							
+							Log.log("\nNo engineers left. Nodes are still pending for review. Fail!\n");
+							System.out.println(numNodesReviewed + " total nodes reviewed");
+							return 0;
+						}
 					}
 				}
 				else {
+					Log.log("No reviews left for node " + nodeIndex + ", skipping...");
 					numNodesReviewed++;
-					System.out.println("nodes reviewed = " + numNodesReviewed + " Total nodes = " + components.size());
-					System.out.println("review remaining = " + currentNode.reviewsLeft);
 					if (numNodesReviewed == components.size()) {
 						isDone = true;
 						return 1;
 					}
 				}
+
 				nodeIndex++;
 				if (nodeIndex >= components.size()) {
-					nodeIndex = 0;
+					Log.log("Too many nodes to review. There are still components left to review. Fail");
+					return 0;
 				}
 			}
 		}
@@ -115,14 +138,23 @@ public class ProblemB2012 {
 		try {
 			BufferedReader br = new BufferedReader(new FileReader("input.txt"));
 			String line = br.readLine();
+			int i = 0;
 			while (!line.equals("0 0") && line != null) {
-				System.out.println(problem.init(line, br));
+				i++;
+				Log.log("\n\nNEW PROBLEM " + i + "\n\n");
+				Log.log(problem.init(line, br));
 				line = br.readLine();
 			}
 		}
 		catch(Exception e) {
 			e.printStackTrace();
 		}
+	}
+}
+
+class Log {
+	public static void log(Object text) {
+		// System.out.println(text);
 	}
 }
 
@@ -133,8 +165,8 @@ class CompNode {
 	public CompNode next;
 
 	public CompNode(int numReviews) {
-		totalReviews = 0;
-		reviewsLeft = 0;
+		totalReviews = numReviews;
+		reviewsLeft = numReviews;
 		next = null;
 	}
 }
@@ -148,8 +180,8 @@ class EngNode {
 	public ArrayList<CompNode> visitedComps;
 
 	public EngNode(int numReviews) {
-		totalReviews = 0;
-		reviewsLeft = 0;
+		totalReviews = numReviews;
+		reviewsLeft = numReviews;
 		next = null;
 		visitedComps = new ArrayList<CompNode>();
 	}
